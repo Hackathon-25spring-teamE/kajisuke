c17d1c43084a15250163f1b780a4144271469b50
mimi-2023 <karasu@LAPTOP-L90LNOLE>
2025-04-23 01:13:15 +0900

    appsアプリケーション作成、helloworld作成、templates、staticディレクトリ追加、settings.py基本設定

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
-# poetry run python manage.py migrate --noinput
+
+# DBのマイグレーション
+poetry run python manage.py migrate --noinput
+
 # poetry run python manage.py collectstatic --noinput
-poetry run python manage.py runserver 0.0.0.0:8000
+
+# サーバー起動の代わりに常駐プロセスとして待機
+tail -f /dev/null
+
+# 開発サーバーの起動
+# poetry run python manage.py runserver 0.0.0.0:8000
--- kajisuke/settings.py
+++ kajisuke/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 作成したアプリケーションを追加
+    'apps.apps.AppsConfig',
 ]
 
 MIDDLEWARE = [
@@ -62,7 +64,7 @@ ROOT_URLCONF = 'kajisuke.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,9 +124,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
