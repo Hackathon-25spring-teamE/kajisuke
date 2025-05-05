235d9529203d3c7625cea9cd3bc7c068b0bd420b
mimi-2023 <138312444+mimi-2023@users.noreply.github.com>
2025-05-05 14:17:58 +0900

    admin.pyに管理画面でカスタムユーザーモデルを使うための設定を追加。管理画面からユーザー作成ができることを確認済み

--- apps/admin.py
+++ apps/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
 # Register your models here.
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('email', 'user_name', 'is_staff')
+    list_filter = ('is_staff', 'is_superuser')
+    search_fields = ('email', 'user_name')
+    ordering = ('email',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'user_name', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Dates', {'fields': ('last_login',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'user_name', 'password1', 'password2', 'is_staff')}
+        ),
+    )
