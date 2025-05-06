39dce8ed8c5aac4899e4854d407ea56d59027e8a
Mkottonn <papupim@outlook.jp>
2025-05-06 13:35:43 +0000

    urls.pyを修正

--- apps/urls.py
+++ apps/urls.py
@@ -1,14 +1,11 @@
 from django.urls import path
-from .views import hello_world
-
-
 from . import views
 
 app_name = "apps"
 
 urlpatterns = [
-    path('', hello_world, name="hello_world"),
-    path('signup/', views.SignupView.as_view(), name="signup"),
-    path('signin/', views.SigninView.as_view(), name="signin"),
-    path('signout/', views.SignoutView.as_view(), name="signout")
+    path('', views.hello_world, name="hello_world"),
+    path('signup', views.SignupView.as_view(), name="signup"),
+    path('signin', views.SigninView.as_view(), name="signin"),
+    path('signout', views.SignoutView.as_view(), name="signout")
 ]
