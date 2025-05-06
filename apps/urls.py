40c70224f306f69cb2f9f9ab7604721a4d3911d2
mimi-2023 <138312444+mimi-2023@users.noreply.github.com>
2025-05-06 18:50:35 +0900

    FullCalendar導入。使用テスト中

--- apps/urls.py
+++ apps/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 from .views.auth_views import SigninView, SignupView, SignoutView, hello_world
 
 app_name = "apps"
+from .views import calendar_month, calendar_day, event_list
 
 urlpatterns = [
-    # テスト用ページ
-    path('', hello_world, name="hello_world"),
-    # 認証機能
-    path('signup', SignupView.as_view(), name="signup"),
-    path('signin', SigninView.as_view(), name="signin"),
-    path('signout', SignoutView.as_view(), name="signout")
+    path('calendars/<int:year>/<int:month>/<int:day>', calendar_day, name='calendar_day'),
+    path('calendars/', calendar_month, name='calendar_view'),
+    path('api/events/', event_list, name='event_list'),
+    path('', views.hello_world, name="hello_world"),
+    path('signup', views.SignupView.as_view(), name="signup"),
+    path('signin', views.SigninView.as_view(), name="signin"),
+    path('signout', views.SignoutView.as_view(), name="signout")
 ]
