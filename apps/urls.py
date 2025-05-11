9c519038faf51684a99cb4e38d83f231da7aaab8
mimi-2023 <138312444+mimi-2023@users.noreply.github.com>
2025-05-11 13:56:57 +0900

    developを反映するために調整

--- apps/urls.py
+++ apps/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path
-from .views.auth_views import SigninView, SignupView, SignoutView, hello_world
-
-app_name = "apps"
+from apps.views.auth_views import SigninView, SignupView, SignoutView, hello_world
 from apps.views.calendar_views import calendar_month, calendar_day, task_list
 
+
+app_name = "apps"
+
 urlpatterns = [
+    path('', hello_world, name="hello_world"),
+    path('signup', SignupView.as_view(), name="signup"),
+    path('signin', SigninView.as_view(), name="signin"),
+    path('signout', SignoutView.as_view(), name="signout"),
     path('calendars/<int:year>/<int:month>/<int:day>', calendar_day, name='calendar_day'),
     path('calendars/', calendar_month, name='calendar_view'),
     path('api/tasks/', task_list, name='task_list'),
-    # path('', views.hello_world, name="hello_world"),
-    path('signup', views.SignupView.as_view(), name="signup"),
-    path('signin', views.SigninView.as_view(), name="signin"),
-    path('signout', views.SignoutView.as_view(), name="signout")
 ]
