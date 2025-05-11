c31790cb4d0b59dff72a5d19819cf4fafd1a88cd
mimi-2023 <138312444+mimi-2023@users.noreply.github.com>
2025-05-12 01:47:57 +0900

    dateutil.rrule追加。繰り返しの書き出しを作成

--- apps/views/calendar_views.py
+++ apps/views/calendar_views.py
@@ -4,7 +4,8 @@ from django.http import JsonResponse
 from django.utils import timezone
 from datetime import datetime
 from zoneinfo import ZoneInfo
-from ..models import PastSchedule
+from dateutil.rrule import rruleset, rrule, DAILY, WEEKLY, MONTHLY, YEARLY, MO, TU, WE, TH, FR, SA, SU
+from ..models import PastSchedule, Schedule
 
 
 # カレンダー表示
@@ -17,27 +18,81 @@ def task_list(request):
     # ユーザーのTZでの今日の日付を取得する
     user_tz = ZoneInfo(request.GET["timeZone"])
     today = timezone.now().astimezone(user_tz).date()
-    # カレンダー表示の開始日と終了日を取得する
-    calendar_start_date = datetime.fromisoformat(request.GET["start"]).date()
-    calendar_end_date = datetime.fromisoformat(request.GET["end"]).date()
+    # カレンダー表示の開始日と終了日をdatetimeで取得する
+    calendar_start = datetime.fromisoformat(request.GET["start"])
+    calendar_end = datetime.fromisoformat(request.GET["end"])
+
+    schedules_list = []
 
     # 1. today>calendar_start_dateなら、past_schedulesから対象userのcalendar_start_dateからtoday前までのレコードを取得する
-    if today > calendar_start_date:
+    if today > calendar_start.date():
         # past_schedulesとschedules,tasks,task_categoriesをjoinして、対象ユーザーの対象期間のレコードを取得する
         past_schedules = PastSchedule.objects.filter(
             schedule__user=request.user, 
-            schedule_date__gte=calendar_start_date
+            schedule_date__gte=calendar_start.date()
         ).select_related(
             'schedule__task',
             'schedule__task__task_category'
         ).order_by('schedule_date') 
-        print(past_schedules)
+        # print(past_schedules)
 
-
-
+        for item in past_schedules:
+            category_settings = get_category_settings(item.schedule.task.task_category.id)
+            schedule = {
+                "title": category_settings["icon"] + item.schedule.task.task_name, 
+                "start": item.schedule_date, 
+                "end": item.schedule_date,
+                "allDay": True,
+                'textColor': "#333333",
+                'backgroundColor': category_settings["color"], 
+            }
+            schedules_list.append(schedule)
 
     # 2. today<=calendar_end_dateなら、schedulesから対象userのis_active=True,start_date<=calendar_end_dateのレコードを取得する
-    # 2-1. schedulesの繰り返しから、todayからcalendar_end_dateまでの期間の日付リストを作成する
+    if today <= calendar_end.date():
+        # schedulesとtasks,task_categoriesをjoinして、対象ユーザーの対象期間のレコードを取得する
+        future_schedules = Schedule.objects.filter(
+            user=request.user, 
+            start_date__lte=calendar_end.date(),
+            is_active=True
+        ).select_related(
+            'task',
+            'task__task_category'
+        ).order_by('start_date') 
+        # print(future_schedules)
+        # print(future_schedules[0].task.task_name)
+
+        # frequency を文字列からdateutil.rruleの定数に変換するための辞書
+        frequency_map = { "DAILY": DAILY,"WEEKLY": WEEKLY,"MONTHLY": MONTHLY,"YEARLY,": YEARLY }
+        # 曜日を文字列からdateutil.rruleの曜日オブジェクトに変換するための辞書
+        weekday_map = { "MO": MO,"TU": TU,"WE": WE,"TH": TH,"FR": FR,"SA": SA,"SU": SU }
+
+        # 2-1. schedulesの繰り返しから、todayからcalendar_end_dateまでの期間の日付リストを作成する
+        for item in future_schedules:
+            # frequencyがNONEの時はstart_dateのみ日付リストに追加
+            if item.frequency == "NONE":
+                date_list = []
+                date_list.append(item.start_date)
+            else:
+                date_list = rruleset()
+                reccurences = {
+                    "freq": frequency_map.get(item.frequency), 
+                    "interval": item.interval, 
+                    "dtstart": item.start_date,
+                    "byweekday": weekday_map.get(item.day_of_week),
+                    "bymonthday": item.day_of_month,
+                    "bysetpos": item.nth_weekday,
+                }
+                # print(item.task.task_name, reccurences)
+                filted_reccurences = { k: v for k, v in reccurences.items() if v is not None }
+                # print(item.task.task_name, filted_reccurences)
+                # print(calendar_start_date)
+                
+                date_list.rrule(
+                    rrule(**filted_reccurences)
+                    .between(calendar_start, calendar_end, inc=True)
+                    )
+                # print(item.task.task_name, list(date_list))
     # 2-2. exptional_schedulesから対象schedule_idの2つのdateのどちらかがtodayからcalendar_end_dateまでの期間に入っているレコードを取得する
     # 2-3. 日付リストに対し、上記レコードのoriginal_dateを除外（exdate）し、modified_dateを追加（rdate）する
     # 2-4. schedule_id毎に作成した日付リストを、各スケジュール情報を持たせた状態で一つのリストにまとめる
@@ -46,7 +101,7 @@ def task_list(request):
 
     # events = Event.objects.all()
     # data = [event.as_dict() for event in events]
-    data = [
+    test_data = [
         {"title":"🧹掃除機", # task_categoryごとの絵文字+task_name
          "start":'2025-05-01', # schedule_date or 
          "end":'2025-05-01',
@@ -73,7 +128,7 @@ def task_list(request):
         },
         
         ]
-    return JsonResponse(data, safe=False)
+    return JsonResponse(schedules_list, safe=False)
 
 
 
@@ -86,3 +141,18 @@ def calendar_day(request, year, month, day):
     }
     return render(request, 'dev/dev.html', {'context': context})
 
+
+# category_idに対応するiconとcolorを返す
+def get_category_settings(category_id):
+    category_dict = {
+        1 : { "icon" : "🧹", "color" : "#C5D7FB"},
+        2 : { "icon" : "🍳", "color" : "#FFE380"},
+        3 : { "icon" : "🧺", "color" : "#9BD4B5"},
+        4 : { "icon" : "🗂", "color" : "#C0F354"},
+        5 : { "icon" : "🌸", "color" : "#99F2FF"},
+        6 : { "icon" : "🛠", "color" : "#FFC199"},
+    }
+    if category_id in category_dict:
+        return category_dict[category_id]
+    else:
+        return None
