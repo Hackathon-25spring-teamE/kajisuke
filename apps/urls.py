07863dc2851e7f34f33ff72357f094c2d9ca6222
Mkottonn <papupim@outlook.jp>
2025-05-12 21:29:36 +0900

    URL追加

--- apps/urls.py
+++ apps/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views.auth_views import SigninView, SignupView, SignoutView, hello_world
 from .views.calendar_views import calendar_month, calendar_day, schedules_list
+from .views.schedules_views import ScheduleCreateView, load_tasks
+
+
 
 
 app_name = "apps"
@@ -16,4 +19,7 @@ urlpatterns = [
     path('calendars/<int:year>/<int:month>/<int:day>', calendar_day, name='calendar_day'),
     path('calendars/<int:year>/<int:month>', calendar_month, name='calendar_month'),
     path('api/schedules', schedules_list, name='schedules_list'),
+    #スケジュール登録
+    path('schedules/create', ScheduleCreateView.as_view(), name="schedules_create"),
+    path('ajax/load-tasks', load_tasks, name='ajax_load_tasks'),
 ]
