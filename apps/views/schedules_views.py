d884ab0efc6fdbcb73a663b9f4755144ac80a12f
Mkottonn <papupim@outlook.jp>
2025-05-12 21:38:35 +0900

    カテゴリーにひもづくタスクの取得関数追加

--- apps/views/schedules_views.py
+++ apps/views/schedules_views.py
@@ -2,11 +2,12 @@ from django.views.generic.edit import CreateView
 from django.http import JsonResponse
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Schedule, Task
-from .forms import ScheduleForm
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 
-
+from ..models import Schedule, Task
+from ..forms import ScheduleForm
 
 
 
@@ -33,6 +34,14 @@ def load_tasks(request):
     user = request.user
     tasks = Task.objects.filter(task_category_id=category_id).filter(user__in=[None, user])
     return JsonResponse(list(tasks.values('id', 'task_name')), safe=False)
+
+#カテゴリーにひもづくタスクの取得
+@login_required
+def get_tasks_by_category(request):
+    category_id = request.GET.get('category_id')
+    tasks = Task.objects.filter(task_category_id=category_id).filter(user__in=[None, request.user])
+    task_list = [{'id': task.id, 'name': task.task_name} for task in tasks]
+    return JsonResponse({'tasks': task_list})
 # スケジュール変更・削除
 
 # スケジュール実施・未実施
