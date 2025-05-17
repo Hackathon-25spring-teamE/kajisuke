5b42fca2ded6db15a213144f0726b73e33229778
mai051 <aco_miya@outlook.jp>
2025-05-17 15:19:04 +0900

    developからpull

---
+++ apps/views/auth_views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render ,redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, logout, authenticate
+from django.views.generic import CreateView
+from django.contrib.auth.views import LoginView as BaseLoginView, LogoutView as BaseLogoutView
+from django.urls import reverse_lazy
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import View
+
+
+from ..forms import SignupForm, SigninForm
+
+# Create your views here.
+
+# テスト用表示
+def hello_world(request):
+    context = {
+        "message1": "Hello, world.",
+        "message2": "KAJISUKEapp is here!",
+    }
+    # フロントページができたら、以下の形に書き換える（contextは直接渡す）
+    # return render(request, '<フロントページのhtml>', context)
+    return render(request, 'dev/dev.html', {'context': context})
+
+
+# サインアップ
+class SignupView(CreateView):
+    form_class = SignupForm
+    template_name = "dev/signup.html"
+    success_url = reverse_lazy("apps:hello_world")
+
+    def form_valid(self, form):
+        # signin after signup
+        response = super().form_valid(form)
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password1")
+        user = authenticate(email=email, password=password)
+        
+        if user is not None:
+            login(self.request, user)
+            return response
+        else:
+            # ログインに失敗した場合、新規登録ページへリダイレクト
+            return HttpResponseRedirect(reverse("signup"))
+
+            
+# サインイン
+class SigninView(BaseLoginView):
+    form_class = SigninForm
+    template_name = "dev/signin.html"
+
+    def form_valid(self, form):
+        # 明示的にログイン
+        login(self.request, form.get_user())
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('apps:hello_world')
+
+
+# サインアウト
+class SignoutView(View):
+    def get(self, request):
+        logout(request)
+        return redirect('apps:signin')
