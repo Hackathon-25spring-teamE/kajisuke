66379b69df93328317d8e051bfdf6a630a68c368
mimi-2023 <138312444+mimi-2023@users.noreply.github.com>
2025-05-17 12:39:30 +0900

    month.htmlのベースをbase.htmlへ変更。css,js調整。headertoolbar非表示に変更

--- static/js/month.js
+++ static/js/month.js
@@ -1,48 +1,48 @@
 document.addEventListener('DOMContentLoaded', function() {
   const calendarEl = document.getElementById('calendar');
-
   const calendar = new FullCalendar.Calendar(calendarEl, {
-      views: {
-      dayGridMonth: {
-        titleFormat: { month: "long" }
-      }
+    views: {
+        dayGridMonth: {
+        // titleFormat: { month: "long" }
+        }
     },
-      headerToolbar: {
-          left: 'prev',
-          center: 'title',
-          right: 'next'
-          },
-      events: '/api/schedules',
-      timeZone: "Asia/Tokyo", //念のため
-      locale: "jp",
-      displayEventTime: false,  // 時間表示を消す設定
-      dayMaxEventRows: true,
-      height: 650, 
-      eventBorderColor: 'transparent', // ボーダーの色を透明にする
-      dayCellContent: function(arg){
-          return arg.date.getDate();
-      },
-      // ポップアップを開かないように明示
-      eventLimitClick: false,  // ← この設定は古いが残っていれば追加
-      moreLinkClick: function() {
-          return 'none';         // ← 「+N件」クリック時も無反応にする
-      },
-      selectable: false,  // ← 選択機能を無効化
-      eventClick: function(info) {
-          info.jsEvent.preventDefault(); // デフォルト動作無効
-          info.el.blur(); // 明示的に focus を外す
-      },
+    // headerToolbar: {
+    //     left: 'prev',
+    //     center: 'title',
+    //     right: 'next'
+    //     },
+    headerToolbar: false,
+    events: '/api/schedules',   // スケジュールリストのリクエスト
+    timeZone: "Asia/Tokyo",
+    locale: "jp",
+    displayEventTime: false,  // 時間表示を消す設定
+    dayMaxEventRows: true,
+    height: 650, 
+    eventBorderColor: 'transparent', // イベントボーダーの色を透明にする
+    // 日付から「日」表記を外す
+    dayCellContent: function(arg){
+        return arg.date.getDate();
+    },
+    // ポップアップを開かないように明示
+    eventLimitClick: false,  // ← この設定は古いが残っていれば追加
+    moreLinkClick: function() {
+        return 'none';         // ← 「+N件」クリック時も無反応にする
+    },
+    selectable: false,  // ← 選択機能を無効化
+    eventClick: function(info) {
+        info.jsEvent.preventDefault(); // デフォルト動作無効
+        info.el.blur(); // 明示的に focus を外す
+    },
+    // 日付クリック時にURL遷移
+    dateClick: function(info) {
+        const date = new Date(info.dateStr);  // クリックされた日付
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0'); // 月は0-index
+        const day = String(date.getDate()).padStart(2, '0');
 
-      // 日付クリック時にURL遷移
-      dateClick: function(info) {
-          const date = new Date(info.dateStr);  // クリックされた日付
-          const year = date.getFullYear();
-          const month = String(date.getMonth() + 1).padStart(2, '0'); // 月は0-index
-          const day = String(date.getDate()).padStart(2, '0');
-
-          const url = `/calendars/${year}/${month}/${day}`;
-          window.location.href = url;
-      }
+        const url = `/calendars/${year}/${month}/${day}`;
+        window.location.href = url;
+    }
   });
 
   calendar.render();
