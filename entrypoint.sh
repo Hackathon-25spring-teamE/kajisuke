74fcf60f2d4afc2ab5730a0a0af1d017a91e326f
mimi-2023 <138312444+mimi-2023@users.noreply.github.com>
2025-05-25 02:29:57 +0900

    微修正

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ poetry run python manage.py migrate --noinput
 
 # 開発サーバーの起動
 # 起動したくないときはコメントアウト→下のtail～が代わりに常駐プロセスとして待機する
-# poetry run python manage.py runserver 0.0.0.0:8000
+poetry run python manage.py runserver 0.0.0.0:8000
 
 # サーバー起動の代わりに常駐プロセスとして待機
 tail -f /dev/null
