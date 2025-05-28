4d8207652ee068df8b0a0abed4ec50411d412728
mai051 <aco_miya@outlook.jp>
2025-05-28 19:53:50 +0900

    バッチ処理がEC2の片方だけで動くように設定

--- apps/management/commands/runapscheduler.py
+++ apps/management/commands/runapscheduler.py
@@ -1,3 +1,4 @@
+import os
 from django.core.management.base import BaseCommand
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apps.batch import insert_past_schedules
@@ -6,6 +7,12 @@ class Command(BaseCommand):
     help = 'Run APScheduler using BlockingScheduler'
 
     def handle(self, *args, **kwargs):
+        batch_enabled = os.getenv("BATCH_ENABLED", "false").lower() == "true"
+
+        if not batch_enabled:
+            self.stdout.write('[INFO] BATCH_ENABLED is not true. Skipping scheduler.')
+            return
+
         scheduler = BlockingScheduler()
         scheduler.add_job(insert_past_schedules, 'cron', hour=0, minute=0)
         self.stdout.write('Scheduler started.')
