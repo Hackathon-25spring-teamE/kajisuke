8ada808c83fe7cbbbe6070f74a274e910487d566
mai051 <aco_miya@outlook.jp>
2025-05-30 15:37:48 +0900

    settings.pyの不要なコードの削除

--- kajisuke/settings.py
+++ kajisuke/settings.py
@@ -23,9 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEYを.envから取得
-# SECRET_KEY = os.environ.get("SECRET_KEY")
 
 def get_secret_key():
 
